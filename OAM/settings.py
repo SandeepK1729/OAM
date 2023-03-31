68571616b08e3a37d21683f980fcb142211e7979
SandeepK1729 <sandeepkumargalipelly@gmail.com>
2023-03-31 16:25:47 +0530

    deploy patch 1

--- OAM/settings.py
+++ OAM/settings.py
@@ -36,11 +36,13 @@ INSTALLED_APPS = [
 
     'core.apps.CoreConfig',
     'att.apps.AttConfig',
-    'crispy_forms',
-
+    "crispy_forms",
+    "crispy_bootstrap5",
 ]
 
-CRISPY_TEMPLATE_PACK = 'bootstrap4'
+CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
+
+CRISPY_TEMPLATE_PACK = "bootstrap5"
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
