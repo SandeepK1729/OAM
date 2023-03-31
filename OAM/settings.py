c035d1df425043bc3028e085d208e601222b8815
SandeepK1729 <sandeepkumargalipelly@gmail.com>
2023-03-31 18:33:03 +0530

    .

--- OAM/settings.py
+++ OAM/settings.py
@@ -90,7 +90,14 @@ WSGI_APPLICATION = 'OAM.wsgi.application'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 DATABASES = {
-    'default': dj_database_url.parse(env('DATABASE_URL')) # if not DEBUG else sqlitedb
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': env("DATABASE_NAME"),
+        'USER': env("DATABASE_USER"),
+        'PASSWORD': env("DATABASE_PASSWORD"),
+        'HOST': env("DATABASE_HOST"),
+        'PORT': env("DATABASE_PORT"),
+    }
 }
 
 # Password validation
